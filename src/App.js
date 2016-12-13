7e956c7a9bc328f263524dde9f10d9e7516f4f6e
Adam Beguelin <adam@beguelin.com>
2016-12-13 13:40:41 -0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Enter your again to compute your target heart rate.</h2>
+          <h2>Enter your age to compute your target heart rate.</h2>
         </div>
 		<p />
 		<AgeForm />
